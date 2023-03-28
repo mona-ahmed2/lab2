e7d20a2244fc881e3b4c82cbe4bceee1906d1033
mona-ahmed2 <monalisa1998.aa@gmail.com>
2023-03-28 15:42:01 +0300

    done

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+Button b ;
+EditText e ;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        b=(Button)findViewById(R.id.sendbutton);
+        e=findViewById(R.id.personal);
+        b.setOnClickListener(new View.OnClickListener(){
+
+
+
+   public void onClick(View view ){
+       String s =e.getText().toString();
+       Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+       intent.putExtra( "personal",s);
+
+       startActivity(intent);
+
+
+
+   }
+});
+        }}
---
+++ app/src/main/java/com/example/lab2/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent i = getIntent();
+        String s = i.getStringExtra("personal");
+        TextView tv =findViewById(R.id.msg);
+        tv.setText("hello "+s+" !");
+
+
+
+    }
+}
